72041b2785de0d4eedf2968196556f5c9de45530
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-25 11:18:17 +0100

    Added inc/footer.php

---
+++ inc/footer.inc.php
@@ -0,0 +1,9 @@
+<!-- start footer -->
+<footer>
+    <ul>
+        <li><a href="#">Over ons</a></li>
+        <li><a href="#">Privacy</a></li>
+        <li><a href="#">Voorwaarden</a></li>
+    </ul>
+</footer>
+<!-- einde footer -->
