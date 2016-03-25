abcebf0809d2e09055a7f3962996070f5370127f
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-25 11:42:32 +0100

    Modified ogp

---
+++ inc/open-graph-protocol.inc.php
@@ -0,0 +1,16 @@
+<!-- start open graph protocol -->
+
+    <!-- Facebook -->
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:description" content="" />
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="" />
+
+<!-- einde open graph protocol -->
