66574ea00bc8c93b441eceda1d09decbe4b71172
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-25 17:51:15 +0100

    Completed sessiecontrole.inc.php

--- inc/sessiecontrole.inc.php
+++ inc/sessiecontrole.inc.php
@@ -1,10 +1,25 @@
 <?php
 session_start();
 
-//sessiecontrole
-if(!isset($_SESSION["loggedin"])){
-    //live server: header('location: /login.php');
-    header('location: /imdstagram/login.php'); //offline werken in map imdstagram
-}
+/* Voorwaarden succesvol inloggen:
+ * - sessie login bestaat en is niet NULL
+ * - sessie login is niet leeg
+ * - sessie login met key loggedin is true
+ *
+ * Indien bovenstaande voorwaarden voldaan, dan mag bezoeker op de pagina blijven tenzij:
+ * de huidige pagina login.php of register.php is. In dat geval doorsturen naar index page.
+ *
+ * Indien login niet succesvol is, dan wordt gebruiker naar login.php gestuurd tenzij hij zelf op login.php of register.php is
+*/
 
-?>
+$huidige_pagina = basename($_SERVER['PHP_SELF']);
+
+if(isset($_SESSION['login']) && !empty($_SESSION['login']) && $_SESSION['login']['loggedin'] === true){
+    if($huidige_pagina == 'login.php' || $huidige_pagina == 'register.php'){
+        header('location: index.php');
+    }
+}else{
+    if($huidige_pagina != 'login.php' || $huidige_pagina != 'register.php'){
+        header('location: login.php');
+    }
+}
