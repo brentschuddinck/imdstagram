e2fc3d1e870892dccb64dc648844e01a8d8696d5
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-25 11:19:14 +0100

    Added inc/header.php

---
+++ inc/header.inc.php
@@ -0,0 +1,17 @@
+<!-- start header -->
+<header>
+    <nav>
+        <div id="logo"><a href="/index.php">IMDstagram</a></div>
+        <!-- start search
+            <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                <label for="search">Zoeken</label>
+                <input type="search" name="search" placeholder="Zoeken">
+            </form>
+            einde search -->
+        <ul>
+            <li><a href="#">Meldingen</a></li>
+            <li><a href="#">Profiel</a></li>
+        </ul>
+    </nav>
+</header>
+<!-- einde header -->
