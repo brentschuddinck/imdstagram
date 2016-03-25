635434dca33fe86ccc22f2fca3ccb888e2b559ca
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-25 11:22:16 +0100

    Added base template with includes index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include_once('inc/sessiecontrole.inc.php');
+
+?><!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>IMDstagram tijdlijn</title>
+    <meta name="description" content="IMDstagram is dé creative place to be voor IMD studenten. IMDstagram geeft je inspiratie een boost door creatieve en inspirerende afbeeldingen te delen met andere studenten.">
+    <?php include_once('inc/style.inc.php'); ?>
+</head>
+<body>
+
+<?php include_once('inc/header.inc.php'); ?>
+
+
+<!-- start tijdlijn -->
+<!-- einde tijdlijn -->
+
+
+<?php include_once('inc/footer.inc.php'); ?>
+
+</body>
+</html>
