dd44ca97fb570d9ac7194d5a4ddc2b1fffaf59d6
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-26 02:40:13 +0100

    Creating a safe account in db is possible

---
+++ classes/User.class.php
@@ -0,0 +1,95 @@
+<?php
+
+class User{
+
+    //membervariabelen
+
+    private $m_sVoornaamFamilienaam;
+    private $m_sEmailadres;
+    private $m_sGebruikersnaam;
+    private $m_sWachtwoord;
+
+
+    //setters
+
+    public function setMSVoornaamFamilienaam($m_sVoornaamFamilienaam)
+    {
+        $this->m_sVoornaamFamilienaam = $m_sVoornaamFamilienaam;
+    }
+
+
+    public function setMSEmailadres($m_sEmailadres)
+    {
+        $this->m_sEmailadres = $m_sEmailadres;
+    }
+
+
+    public function setMSGebruikersnaam($m_sGebruikersnaam)
+    {
+        $this->m_sGebruikersnaam = $m_sGebruikersnaam;
+    }
+
+
+    public function setMSWachtwoord($m_sWachtwoord)
+    {
+        $this->m_sWachtwoord = $m_sWachtwoord;
+    }
+
+
+
+    //getters
+
+    public function getMSVoornaamFamilienaam()
+    {
+        return $this->m_sVoornaamFamilienaam;
+    }
+
+
+    public function getMSEmailadres()
+    {
+        return $this->m_sEmailadres;
+    }
+
+
+    public function getMSGebruikersnaam()
+    {
+        return $this->m_sGebruikersnaam;
+    }
+
+
+    public function getMSWachtwoord()
+    {
+        return $this->m_sWachtwoord;
+    }
+
+
+
+
+    //functies
+
+    public function Registreer() {
+
+        //connectie db
+        $conn = Db::getInstance();
+
+        //statement voorbereiden
+        $statement = $conn->prepare("INSERT INTO user (full_name, username, email, password) VALUES (:fullname, :username, :email, :password)");
+
+        //values binden
+        $statement->bindValue(":fullname", $this->m_sVoornaamFamilienaam, PDO::PARAM_STR);
+        $statement->bindValue(":username", $this->m_sGebruikersnaam, PDO::PARAM_STR);
+        $statement->bindValue(":email", $this->m_sEmailadres, PDO::PARAM_STR);
+        $statement->bindValue(":password", $this->m_sWachtwoord, PDO::PARAM_STR);
+
+        //statement uitvoeren
+        if($statement->execute()){
+
+        }else{
+            throw new Exception('Ow... er is iets foutgelopen. Je account is niet aangemaakt. Probeer het later opnieuw.');
+        }
+
+    }
+
+}
+
+?>
--- register.php
+++ register.php
@@ -1,5 +1,43 @@
 <?php
 include_once('inc/sessiecontrole.inc.php');
+
+//autoload classes
+spl_autoload_register(function ($class_name) {
+    include 'classes/' .$class_name . '.class.php';
+});
+
+
+if(isset($_POST['registreer']) && !empty($_POST['registreer'])){
+
+    //invoervariabelen
+    $voornaamfamilienaam = $_POST['naam'];
+    $emailadres = $_POST['email'];
+    $gebruikersnaam = $_POST['gebruikersnaam'];
+
+
+    //account valideren en registreren
+    $opties = ['cost' => 12];
+    $wachtwoord = password_hash($_POST['wachtwoord'], PASSWORD_DEFAULT, $opties);
+
+
+    //probeer account te valideren, registreren en toon feedback
+    try {
+        $user = new User();
+
+        $user->setMSVoornaamFamilienaam($voornaamfamilienaam);
+        $user->setMSGebruikersnaam($gebruikersnaam);
+        $user->setMSEmailadres($emailadres);
+        $user->setMSWachtwoord($wachtwoord);
+        $user->Registreer();
+        $feedback = "Yeah! Je account is aangemaakt. <a href='login.php'>Log hier in</a>.";
+
+    } catch(Exception $e) {
+        $feedback = $e->getMessage();
+    }
+
+    echo $feedback;
+
+}
 ?><!doctype html>
 <html lang="nl">
 <head>
@@ -11,24 +49,24 @@ include_once('inc/sessiecontrole.inc.php');
 <body>
 
 <section>
-    <form action="index.php" method="POST">
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
 
         <label for="naam">Voor- en familienaam</label>
-        <input type="text" name="naam" placeholder="Voor- en familienaam" required>
+        <input type="text" name="naam" id="naam" placeholder="Voor- en familienaam" required autofocus title="Vul je voor- en familienaam in.">
 
         <label for="email">E-mailadres</label>
-        <input type="email" name="email" placeholder="@student.thomasmore.be" required>
+        <input type="email" name="email" id="email" placeholder="r@student.thomasmore.be" required title="Vul je Thomas More e-mailadres in.">
 
         <label for="gebruikersnaam">Gebruikersnaam</label>
-        <input type="text" name="gebruikersnaam" placeholder="Gebruikersnaam" required>
+        <input type="text" name="gebruikersnaam" id="gebruikersnaam" placeholder="Gebruikersnaam" required title="Vul een gebruikersnaam in.">
 
         <label for="wachtwoord">Wachtwoord</label>
-        <input type="password" name="wachtwoord" required>
+        <input type="password" name="wachtwoord" id="wachtwoord" placeholder="Wachtwoord" required title="Kies een wachtwoord (minimaal 6 tekens).">
 
         <input type="submit" name="registreer" value="Registreren">
 
     </form>
 </section>
-
+<?php include_once('inc/footer.inc.php'); ?>
 </body>
 </html>
