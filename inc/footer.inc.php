3c980739085c9b6e577af8855df4c3142b96f4f0
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-27 08:24:47 +0200

    Added footer.inc file (libraries before end body tag)

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,5 +1,5 @@
-<!-- start footer -->
-<footer>
+<!-- start footer
+<footer class="container">
     <ul>
         <li><a href="#">Over ons</a></li>
         <li><a href="#">Privacy</a></li>
@@ -7,3 +7,9 @@
     </ul>
 </footer>
 <!-- einde footer -->
+
+
+<!-- start scripts en libraries om in te laden voor afsluiten body -->
+<script src="js/jquery.min.js"></script>
+<script src="js/flat-ui.min.js"></script>
+<!-- einde scripts en libraries om in te laden voor afsluiten body -->
