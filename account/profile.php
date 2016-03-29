ee9e7e248024a7e13b4bb1c6fd5e4066126403d8
Brent Schuddinck <contact@brentschuddinck.be>
2016-03-29 15:42:42 +0200

    Added querystring and auto redirect correctly to that querystring vs current user in profile.php (link sharable)

--- account/profile.php
+++ account/profile.php
@@ -1,6 +1,19 @@
 <?php
 include_once('../inc/sessiecontrole.inc.php');
 
+//welk profiel opvragen?
+//als querystring user bestaat en de waarde hiervan verschillende is van de gebruikersnaam van de ingelogde gebruiker (sessie), dan wordt een ander profiel bekeken
+if(isset($_GET['user']) && $_GET['user'] != $_SESSION['login']['gebruikersnaam']){
+    $pageTitle = "Profiel " . htmlspecialchars($_GET['user']);}
+//in het andere geval wanneer profile.php bezocht wordt zonder user in de querystring, stuur bezoeker door (link zo deelbaar voor anderen)
+else if(!isset($_GET['user'])){
+    $_GET['user'] = $_SESSION['login']['gebruikersnaam'];
+    header('location: profile.php?user=' . $_GET['user']);
+//in het andere geval wil de ingelogde gebruiker zijn eigen profiel bekijken. Toon gepaste titel.
+}else{
+    $pageTitle = "Mijn profiel";
+}
+
 ?><!doctype html>
 <html lang="nl">
 <head>
@@ -19,7 +32,7 @@ include_once('../inc/sessiecontrole.inc.php');
     <div class="row">
         <div class="col-sm-3 col-md-2"></div>
         <div class="col-sm-6 col-md-8">
-            <h1><?php if(isset($_GET['user']) && $_GET['user'] != $_SESSION['login']['gebruikersnaam']){echo "Profiel " . htmlspecialchars($_GET['user']);}else{echo "Mijn profiel";} ?></h1>
+            <h1><?php echo htmlspecialchars($pageTitle); ?></h1>
             <article class="thumbnail">
                 <img data-src="holder.js/100%x600" alt="...">
                 <div class="caption">
